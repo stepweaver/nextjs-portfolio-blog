be312cebba774e0037d8671c1d0204391c8cd8f4
Stephen <stephen@stepweaver.dev>
2024-11-01 16:53:43 -0400

    feat: implement dynamic card display with add and toggle functionality

--- app/page.js
+++ app/page.js
@@ -1,4 +1,33 @@
+'use client';
+
+import Card from '@/components/card';
+import { useState } from 'react';
+
 export default function Home() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [names, setNames] = useState(['Stephen', 'Mike', 'Ben']);
   const name = 'Stephen';
-  return <div className='bg-gray-700'>Hello, {name}</div>;
+  const handleClick = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const cards =
+    isVisible && names.map((name, index) => <Card key={index}>{name}</Card>);
+
+  const handleAdd = () => {
+    setNames([...names, 'New Name!']);
+  };
+
+  return (
+    <>
+      <div className='p-20 space-y-4'>
+        <div>Hello, {name}</div>
+        {cards}
+        <div className='flex space-x-4'>
+          <button onClick={handleClick}>{isVisible ? 'Hide' : 'Show'}</button>
+          <button onClick={handleAdd}>Add</button>
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ components/card.js
@@ -0,0 +1,5 @@
+export default function Card({ children }) {
+  return (
+    <div className='border rounded-md border-gray-600 p-4'>{children}</div>
+  );
+}
